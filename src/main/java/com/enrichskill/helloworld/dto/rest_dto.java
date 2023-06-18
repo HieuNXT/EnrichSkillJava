cf3b82b700eb560aa8030da22a92b153dfe2c7d8
NguyenHieu <metafakenamington@gmail.com>
2023-06-19 00:34:04 +0700

    commit change

---
+++ src/main/java/com/enrichskill/helloworld/dto/rest_dto.java
@@ -0,0 +1,15 @@
+package com.enrichskill.helloworld.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class rest_dto {
+
+  private String content;
+}
+
+
